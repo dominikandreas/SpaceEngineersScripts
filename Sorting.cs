5582c9c47a83a5a802c1ee13479d2f1bd6c0140b
Dominik Dienlin <dominik@dienlin.net>
2018-08-18 00:05:49 +0200

    updated awesome magic sorting algorithm to rule them all

--- Sorting.cs
+++ Sorting.cs
@@ -14,38 +14,35 @@ using VRage.Game.ObjectBuilders.Definitions;
 using VRage.Game.ModAPI.Ingame;
 using SpaceEngineers.Game.ModAPI.Ingame;
 
-namespace SortingScript
+namespace SortingScript2
 {
     public class Program : MyGridProgram
     {
         #endregion
         //To put your code in a PB copy from this comment...
 
-        DateTime T_SortingStarted;
-        IMyTextPanel lcdPanel;
-        IMyConveyorSorter sorterIn, sorterOut;
-        IMyTimerBlock sorterTimer;
-        int maxLCDLines = 10;
-        int sortDurationMs = 300;
-
-        string lcdContent = "";
-
         public Program()
         {
             Runtime.UpdateFrequency = UpdateFrequency.Update100;
-            SetBlockReferences();
-            log("Program initialized\n\n", clear: true);
         }
 
-        public void Save()
-        {
-            //setSorterMode(false);
-        }
+        // block types to search in
+        List<IMyTerminalBlock> containers = new List<IMyTerminalBlock>();
+        List<IMyTerminalBlock> assemblers = new List<IMyTerminalBlock>();
+        List<IMyTerminalBlock> connectors = new List<IMyTerminalBlock>();
+        List<IMyTerminalBlock> oxyGenerators = new List<IMyTerminalBlock>();
+        List<IMyTerminalBlock> refineries = new List<IMyTerminalBlock>();
 
-        public string fill(string s, int l)
-        {
-            return s.PadRight(l, ' ');
-        }
+
+        // variable list to hold target containers
+        List<IMyTerminalBlock> targetContainers = new List<IMyTerminalBlock>();
+
+        IMyTextPanel lcdPanel;
+        string lcdPanelName = "Sorting-Panel";
+        //contains current lcd content (all of it)
+        string lcdContent = "";
+        bool debugLogging = false;
+        int maxLCDLines = 200;  // maximum number of lines (FIFO - earlier lines get removed)
 
         public T[] SubArray<T>(T[] data, int index, int length)
         {
@@ -54,8 +51,10 @@ namespace SortingScript
             return result;
         }
 
-        public void log(string message, bool clear = false, bool endLine = true)
+        public void log(string message, bool clear = false, bool endLine = true, bool debug = false, bool addTime = true)
         {
+            if (debug && !debugLogging)
+                return;
             if (clear)
                 lcdContent = "";
             var lines = lcdContent.Split('\n');
@@ -71,81 +70,136 @@ namespace SortingScript
 
             lines = SubArray(lines, linesStartOffset, linesToCopy);
             lcdContent = string.Join("\n", lines);
+
+            if (addTime)
+            {
+                var dt = DateTime.Now;
+                message = String.Format("{0:T}", dt) + " " + message;
+            }
+
             lcdContent = lcdContent + message + (endLine ? "\n" : "");
 
-            lcdPanel.WritePublicText(lcdContent);
-        }
-
-        public void setSorterMode(bool enabled)
-        {
-            if (enabled)
-            {
-                T_SortingStarted = DateTime.Now;
-                log("sorting components.. ", endLine: false);
-                sorterOut.Enabled = false;
-                sorterIn.DrainAll = true;
-            }
+            if (lcdPanel != null)
+                lcdPanel.WritePublicText(lcdContent);
             else
+                Echo(message);
+        }
+
+        void Main()
+        {
+            GridTerminalSystem.GetBlocksOfType<IMyCargoContainer>(containers);
+            GridTerminalSystem.GetBlocksOfType<IMyAssembler>(assemblers);
+            GridTerminalSystem.GetBlocksOfType<IMyShipConnector>(connectors);
+            GridTerminalSystem.GetBlocksOfType<IMyGasGenerator>(oxyGenerators);
+            GridTerminalSystem.GetBlocksOfType<IMyRefinery>(refineries);
+
+            lcdPanel = GridTerminalSystem.GetBlockWithName(lcdPanelName) as IMyTextPanel;
+
+            if (lcdPanel == null)
             {
-                sorterIn.DrainAll = false;
-                sorterOut.Enabled = true;
-                log("done");
-            }
-
-        }
-
-        public string checkBlockReferences(List<string> blockNames)
-        {
-            foreach (string name in blockNames)
-                if (GridTerminalSystem.GetBlockWithName(name) == null)
-                    throw new Exception(name + " is null!");
-            return "";
-        }
-
-        public bool SetBlockReferences()
-        {
-            string res = checkBlockReferences(new List<string> { "Sorting-Panel-1", "Component-Sorter-In", "Component-Sorter-Out", "Component-Sorting-Timer" });
-            lcdPanel = GridTerminalSystem.GetBlockWithName("Sorting-Panel-1") as IMyTextPanel;
-
-            sorterIn = GridTerminalSystem.GetBlockWithName("Component-Sorter-In") as IMyConveyorSorter;
-            sorterOut = GridTerminalSystem.GetBlockWithName("Component-Sorter-Out") as IMyConveyorSorter;
-            sorterTimer = GridTerminalSystem.GetBlockWithName("Component-Sorting-Timer") as IMyTimerBlock;
-            return true;
-        }
-
-        public void Main(string argument)
-        {
-            if (!SetBlockReferences())
+                log("panel is null!");
                 return;
+            }
 
-            bool isSorting = sorterIn.DrainAll && (T_SortingStarted != null);
+            log("initialized", debug: true);
 
-            int msPassed = (DateTime.Now - T_SortingStarted).Milliseconds;
-            
-            if (isSorting)
+            SortContainerComponents(containers, containers, 0);
+            // Only process the second inventory of assemblers
+            SortContainerComponents(assemblers, containers, 1);
+            log("number of refineries: " + refineries.Count.ToString());
+            SortContainerComponents(refineries, containers, 1);
+            log("refinery name: " + refineries[0].CustomName);
+            SortContainerComponents(connectors, containers, 0);
+            SortContainerComponents(oxyGenerators, containers, 0);
+
+        }
+
+        IMyTerminalBlock GetContainerWithSpaceLeft(List<IMyTerminalBlock> containers)
+        {
+            foreach (IMyTerminalBlock container in containers)
             {
-                
-                if (msPassed > sortDurationMs)
+                IMyInventory inv = container.GetInventory();
+
+                if (!inv.IsFull)
+                    return container;
+            }
+
+            // Either containers list is empty or all containers are full
+            log("no target containers found or all containers are full!");
+            return null;
+        }
+
+        List<String> GetAcceptedItems(IMyTerminalBlock container)
+        {
+            List<String> items = new List<string>();
+            foreach (var line in container.CustomData.Trim().Split('\n'))
+            {
+                items.Add(line.Trim());
+            }
+            return items;
+        }
+
+        List<IMyTerminalBlock> FilterSortingContainers(List<IMyTerminalBlock> containers, int inventoryIndex = 0)
+        {
+            List<IMyTerminalBlock> filtered = new List<IMyTerminalBlock>();
+            foreach (IMyTerminalBlock container in containers)
+            {
+                if (!container.GetInventory(inventoryIndex).IsFull && container.CustomData.Length > 0)
+                    filtered.Add(container);
+            }
+            return filtered;
+        }
+
+        List<String> GetItemTypesForTransfer(IMyTerminalBlock source, IMyTerminalBlock target)
+        {
+            List<String> typesForTransfer = new List<string>();
+            var sourceItemTypes = GetAcceptedItems(source);
+            var targetItemTypes = GetAcceptedItems(target);
+            foreach (var targetItemType in targetItemTypes)
+            {
+                if (!sourceItemTypes.Contains(targetItemType))
                 {
-                    setSorterMode(enabled: false);
-                    if (sorterTimer.Enabled)
-                    {
-                        sorterTimer.StartCountdown();
-                    }
+                    typesForTransfer.Add(targetItemType);
                 }
-                    
             }
-            else if (argument == "start")
-                setSorterMode(enabled: true);
+            return typesForTransfer;
+        }
 
-            if (argument == "auto")
+        void SortContainerComponents(List<IMyTerminalBlock> sourceBlocks, List<IMyTerminalBlock> targetContainers, int inventoryIndex)
+        {
+            foreach (var source in sourceBlocks)
             {
-                sorterTimer.Enabled = true;
-            }
-            else if (argument == "stop")
-            {
-                setSorterMode(enabled: false);
-                sorterTimer.Enabled = false;
+                foreach (var target in FilterSortingContainers(targetContainers))
+                {
+                    if (source == null || target == null || source == target || source.GetInventory() == null || target.GetInventory() == null)
+                        continue;
+
+                    var typesForTransfer = GetItemTypesForTransfer(source, target);
+
+                    debugLogging = string.Join(", ", typesForTransfer).Contains("Ingot");
+
+                    log("transferring " + string.Join(", ", typesForTransfer) + " between " + source.CustomName + " and " + target.CustomName, debug: true);
+
+                    int itemCount = 0;
+
+                    var sourceItems = source.GetInventory().GetItems();
+                    for (int si = 0; si < sourceItems.Count; si++)
+                    {
+                        var item = sourceItems[si];
+                        log("checking " + item.Content.ToString(), debug: true);
+                        foreach (var targetItemType in typesForTransfer)
+                        {
+                            if (item.Content.ToString().Contains(targetItemType))
+                            {
+                                log(" transferring " + item.Amount + " " + item.Content.TypeId, debug: false);
+                                itemCount += 1;
+                                source.GetInventory().TransferItemTo(target.GetInventory(), si, null, true, null);
+                            }
+                        }
+                    }
+                    if (itemCount > 0)
+                        log(" transferred " + itemCount.ToString() + " items from " + source.CustomName + " to " + target.CustomName);
+                }
             }
 
         }
